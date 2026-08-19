244b2732fccc15d3820d9a2a618bf1c5ad04c0a3
Otto Jung <otto.jung@vauplace.com>
2026-08-19 19:58:30 +0000

    Update omiya-opencode to 1.18.18

--- omiya/packages/opencode.scm
+++ omiya/packages/opencode.scm
@@ -10,7 +10,7 @@
 (define-public omiya-opencode
   (package
    (name "omiya-opencode")
-    (version "1.18.4")
+    (version "1.18.18")
     (source
      (origin
       (method url-fetch)
@@ -21,7 +21,7 @@
         "/opencode-linux-x64.tar.gz"))
       (sha256
        (base32
-        "0x51admqi7dlik7d5kz21g7zjgbh70zxdykwpf4d691jzg1n7d5s"))))
+         "1bravfgirc3nmkb86kd21pq8hw6s1h69ia05k5k571cb84ic5p8c"))))
    (build-system binary-build-system)
    (inputs
     `(("glibc" ,glibc)
